916e3a19cf5d999cb218addbc2dd985cdf9912f2
Sujay Kapadnis <116516771+JUSTSUJAY@users.noreply.github.com>
2024-09-02 22:30:03 +0530

    streamlit secret

--- app/config.py
+++ app/config.py
@@ -1,8 +1,9 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 def load_config():
     return load_dotenv()
 
 def get_groq_api_key():
-    return os.getenv('GROQ_API_KEY')
+    return st.secrets["api_key"]
